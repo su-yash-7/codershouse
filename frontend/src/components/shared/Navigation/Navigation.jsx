d10953229971267f9ed179d731c4d901cbeed58e
su-yash-7 <suyashpradhan7@gmail.com>
2021-08-27 18:05:05 +0530

    Creating Navigation Bar

--- frontend/src/components/shared/Navigation/Navigation.jsx
+++ frontend/src/components/shared/Navigation/Navigation.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Navigation.module.css';
 
 const Navigation = () => {
-  return <div>this is nav</div>;
+  const brandStyle = {
+    color: '#fff',
+    textDecoration: 'none',
+    fontWeight: 'bold',
+    fontSize: '22px',
+    display: 'flex',
+    alignItems: 'center',
+  };
+  const logoText = {
+    marginLeft: '10px',
+  };
+  return (
+    <nav className={`${styles.navbar} container`}>
+      <Link style={brandStyle} to="/">
+        <img src="/images/logo.png" alt="logo" />
+        <span style={logoText}>Codershouse</span>
+      </Link>
+    </nav>
+  );
 };
 
 export default Navigation;
